e8b7a21e70de44fe3de88cd1c9ace7166ca7e0e5
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-06-03 19:12:14 -0300

    add colors

---
+++ lib/src/app/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const color_background_light = Color.fromRGBO(229, 229, 229, 1);
+const color_background_dark = Color.fromRGBO(55, 58, 77, 1);
+const color_font = Color.fromRGBO(55, 58, 77, 1);
+const color_field = Color.fromRGBO(245, 245, 250, 1);
+const color_font_hint = Color.fromRGBO(143, 146, 169, 1);
+const color_buttom_dark = Color.fromRGBO(55, 58, 77, 1);
+const color_buttom_light = Color.fromRGBO(229, 229, 229, 1);
