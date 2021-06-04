27a2ca9e176783821cc7b27ffbc7ab6b6315c95f
lucaswilliamgomes <lucaswilliam133@gmail.com>
2021-06-04 17:53:21 -0300

    create view home_page

--- lib/src/app/colors.dart
+++ lib/src/app/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const color_background_light = Color.fromRGBO(229, 229, 229, 1);
-const color_background_dark = Color.fromRGBO(55, 58, 77, 1);
-const color_font = Color.fromRGBO(55, 58, 77, 1);
-const color_field = Color.fromRGBO(245, 245, 250, 1);
-const color_font_hint = Color.fromRGBO(143, 146, 169, 1);
-const color_buttom_dark = Color.fromRGBO(55, 58, 77, 1);
-const color_buttom_light = Color.fromRGBO(229, 229, 229, 1);
+Color colorBackgroundLight = Color.fromRGBO(255, 255, 255, 1);
+Color colorBackgroundDark = Color.fromRGBO(55, 58, 77, 1);
+Color colorFont = Color.fromRGBO(55, 58, 77, 1);
+Color colorField = Color.fromRGBO(245, 245, 250, 1);
+Color colorFontHint = Color.fromRGBO(143, 146, 169, 1);
+Color colorButtomDark = Color.fromRGBO(55, 58, 77, 1);
+Color colorButtomLight = Color.fromRGBO(229, 229, 229, 1);
--- lib/src/home_page/view.dart
+++ lib/src/home_page/view.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:valores_nutricionais/src/app/colors.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +9,97 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Hello world"),),
-    );
+    return Scaffold(body: LayoutBuilder(builder: (context, constraints) {
+      final _height = constraints.maxHeight;
+      final _width = constraints.maxWidth;
+      return Container(
+        alignment: Alignment.center,
+        color: colorBackgroundLight,
+        child: Form(
+          key: _formKey,
+          child: Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: _height * .3, left: 30),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "ALIMENTO",
+                style: TextStyle(
+                  color: colorFont,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 30, left: 30),
+              child: TextFormField(
+                textAlignVertical: TextAlignVertical.top,
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  fillColor: Color.fromRGBO(242, 242, 242, 1),
+                  filled: true,
+                  hintText: "Arroz",
+                  contentPadding: EdgeInsets.only(left: 20, right: 20),
+                  border: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30, top: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "QUANTIDADE EM GRAMAS",
+                style: TextStyle(
+                  color: colorFont,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 30, left: 30),
+              child: TextFormField(
+                textAlignVertical: TextAlignVertical.top,
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  fillColor: Color.fromRGBO(242, 242, 242, 1),
+                  filled: true,
+                  hintText: "100",
+                  contentPadding: EdgeInsets.only(left: 20, right: 20),
+                  border: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 30, left: 30),
+              child: MaterialButton(
+                height: 45,
+                minWidth: 200,
+                child: Text(
+                  "CONTINUAR",
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+                color: colorButtomDark,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4)),
+                onPressed: () {
+                  setState(() {
+                    _formKey.currentState.save();
+                  });
+                },
+              ),
+            ),
+          ]),
+        ),
+      );
+    }));
   }
 }
